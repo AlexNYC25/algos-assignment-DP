1c3573b2036e81903fe8c999a3a7de1346777482
Alexis Montes <montes.alexis25@gmail.com>
2019-04-02 22:50:57 -0400

    Final Version

--- RodCutting.java
+++ RodCutting.java
@@ -1,5 +1,6 @@
 /**
- * Rod cutting problem described in Chapter 15 of textbook
+ * Rod cutting problem described in Chapter 15 of textbook Completed By Alexis
+ * Montes
  */
 public class RodCutting {
 
